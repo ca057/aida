72cd1c8aeef8e6fb90d2d0c415d0b99ba8122361
Christian Ost <christian@christianost.de>
2019-01-18 10:03:21 +0100

    basic connection between activity and sketch done

--- app/src/main/java/de/christianost/aida/sketch/Sketch.java
+++ app/src/main/java/de/christianost/aida/sketch/Sketch.java
@@ -1,6 +1,7 @@
 package de.christianost.aida.sketch;
 
 public interface Sketch {
+    void setSize(int width, int height);
     // TODO: return an image here
     void toImage();
 }
--- app/src/main/java/de/christianost/aida/sketch/processing/ProcessingSketch.java
+++ app/src/main/java/de/christianost/aida/sketch/processing/ProcessingSketch.java
@@ -4,10 +4,27 @@ import de.christianost.aida.sketch.Sketch;
 import processing.core.PApplet;
 
 public class ProcessingSketch extends PApplet implements Sketch {
-    public ProcessingSketch(){}
+    private int width;
+    private int height;
+
+    public ProcessingSketch(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void settings() {
+        size(this.width, this.height);
+    }
+
+    public void draw() {
+        if (mousePressed) {
+            ellipse(mouseX, mouseY, 50, 50);
+        }
+    }
 
     @Override
     public void toImage() {
         // should return an image
     }
+
 }
