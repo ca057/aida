fe6899258cf4f4bba3fcf0adde76233cca0145e3
Christian Ost <christian@christianost.de>
2019-01-18 09:47:53 +0100

    add processing core as dependency

--- app/src/main/java/de/christianost/aida/sketch/processing/ProcessingSketch.java
+++ app/src/main/java/de/christianost/aida/sketch/processing/ProcessingSketch.java
@@ -1,8 +1,9 @@
 package de.christianost.aida.sketch.processing;
 
 import de.christianost.aida.sketch.Sketch;
+import processing.core.PApplet;
 
-public class ProcessingSketch implements Sketch {
+public class ProcessingSketch extends PApplet implements Sketch {
     public ProcessingSketch(){}
 
     @Override
